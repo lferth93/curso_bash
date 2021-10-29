0afbd61c5dd5e51e65745df7b49f81de6b17b392
lferth93 <luisfer.th93@gmail.com>
2021-10-29 07:21:29 -0500

    For

---
+++ 05P_par.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Escribir un script que reciba numeros
+# enteros como parametros y verifique si 
+# son pares el estado de salida debe 
+# ser el siguiente:
+#
+#	Estado 0 si todos los numeros son pares 
+# 	y no mostrar nada en la terminal.
+# 	
+#	Estado 1 si alguno de los numeros no es 
+# 	par y mostrar el primer numero impar.
